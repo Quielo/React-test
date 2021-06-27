5ece1edd094197585b65c6b1748f7a5ff8b09021
Quielo <mty.gonzalez@gmail.com>
2021-06-27 16:07:52 -0500

    Dev branch test

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Hello from './components/hello-world';
 
 function App() {
   return (
     <div className="App">
-      Hello world!
+      <Hello />
     </div>
   );
 }
---
+++ src/components/hello-world.jsx
@@ -0,0 +1,10 @@
+function Hello() {
+    return(
+        <>
+        Hello World
+        </>
+    );
+    
+}
+
+export default Hello
